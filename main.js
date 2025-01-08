8c6e2531bdeefbe808370f323083a69d06a82a00
DylanHyllan <143070250+DylanHyllan@users.noreply.github.com>
2025-01-08 22:39:05 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const junc = document.getElementById("jung");
+
+junc.addEventListener("click", () => {
+    window.location.href = "carlJung.html";
+});
+
+const homebc = document.getElementById("homeb");
+
+homebc.addEventListener("click", () => {
+    window.location.href = "index.html";
+});
+
