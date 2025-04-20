6b36c48d8a8e810294383c1d610ba56038f2f345
DylanHyllan <143070250+DylanHyllan@users.noreply.github.com>
2025-04-20 19:37:47 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-const junc = document.getElementById("jung");
+document.addEventListener("DOMContentLoaded", () => {
+    const junc = document.getElementById("jung");
+    if (junc) {
+        junc.addEventListener("click", () => {
+            window.location.href = "carlJung.html";
+        });
+    }
 
-junc.addEventListener("click", () => {
-    window.location.href = "carlJung.html";
+    const frec = document.getElementById("freud");
+    if (frec) {
+        frec.addEventListener("click", () => {
+            window.location.href = "sigmundFreud.html";
+        });
+    }
+
+    const homeb = document.getElementById("homeb");
+    if (homeb) {
+        homeb.addEventListener("click", () => {
+            window.location.href = "index.html";
+        });
+    }
 });
-
-const homebc = document.getElementById("homeb");
-
-homebc.addEventListener("click", () => {
-    window.location.href = "index.html";
-});
-
